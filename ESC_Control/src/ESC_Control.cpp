af5bcd5f2dab8bdef96b85fab824a44c6d6c30b2
Brunner <brunner@brunner-Lenovo-B560>
2013-12-29 12:31:52 +0100

    converted to class

--- ESC_Control/src/ESC_Control.cpp
+++ ESC_Control/src/ESC_Control.cpp
@@ -14,22 +14,42 @@
 #include <errno.h>
 #include <termios.h>
 #include <unistd.h>
-
 #include <stdint.h>
 
 #include <iostream>
 #include <sstream>
 
-
 using namespace std;
 
-int fd1;
-int fd2;
-int numberWrittenBytes, nbytes, tries;
-int numberReadBytes;
 
-//TODO: check terminos flags
-int configure_port(int fd)      // configure the port
+class STMCommunicator{
+
+public:
+	STMCommunicator();
+	~STMCommunicator();
+	int configure_port(int fd);
+	int open_STM_board();
+	int writeByte(uint16_t pwmWidth);
+
+private:
+
+	int fd1;
+	int numberWrittenBytes;
+	int numberReadBytes;
+
+	char readBuffer[10];
+
+};
+
+STMCommunicator::STMCommunicator(){
+
+}
+
+STMCommunicator::~STMCommunicator(){
+	close(fd1);
+}
+
+int STMCommunicator::configure_port(int fd)      // configure the port
 {
 	struct termios port_settings;      // structure tiostreamo store the port settings in
 	memset(&port_settings, 0, sizeof(port_settings));
@@ -49,8 +69,7 @@ int configure_port(int fd)      // configure the port
 
 }
 
-int main() {
-
+int STMCommunicator::open_STM_board(){
 	fd1 = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
 
 	if (fd1 == -1){
@@ -65,10 +84,72 @@ int main() {
 	fd1 = configure_port(fd1);
 	fcntl(fd1, F_SETFL, O_NONBLOCK);
 
-//	With following code we send to device some bitsiostream:
+	return 1;
+}
+
+int STMCommunicator::writeByte(uint16_t byteToWrite){
+
+	char writeBytes[1];
+	memcpy((void *)writeBytes, (void*)&byteToWrite, 1);
+
+	bool byteWritten = false;
+	while(!byteWritten){
+		usleep(1000);
+		int numberBytesWritten = write(fd1, (void*)&(writeBytes[0]), 1);
+		if(numberBytesWritten == 1){
+			byteWritten = true;
+		} else {
+			cout << "numberBytesWritten byte is smaller zero: " << numberBytesWritten << endl;
+			return -1;
+		}
+		cout << "write-check ... number bytes written: " << numberBytesWritten << endl;
+	}
+
+	cout << "Try to receive acknowledge byte ...\n";
+
+	bool byteRead = false;
+	while(!byteRead){
+		usleep(10000);
+		int numberBytesRead = read(fd1, (void*)&(readBuffer[0]), 1);
+		if(numberBytesRead > 1){
+			cout << "read more than 2 bytes at the same time!" << endl;
+			return -1 ;
+		}
+		else if(numberBytesRead < 0){
+			cout << "############# error during read ################" << endl;
+			continue;
+		} else if(numberBytesRead == 1){
+			byteRead = true;
+		}
+		if(readBuffer[0] == 10 || readBuffer[0] == 13){
+			cout << "Stop/Start Byte received!" << endl;
+			byteRead = false;
+		}
+		cout << "read-check; numRead is " << numberBytesRead << endl;
+	}
+
+	uint8_t receivedAcknowledgeByte = 0;
+	memcpy((void *)&receivedAcknowledgeByte, (void*)readBuffer, 1);
+
+	if(receivedAcknowledgeByte == byteToWrite){
+		cout << "Same value!" << endl;
+	} else {
+		cout << "Not the same value! receivedByte: " << (uint)(receivedAcknowledgeByte) << endl;
+	}
+
+	return 0;
+
+}
+
+
+int main() {
+
+	STMCommunicator mySTMCommunicator;
+
+	mySTMCommunicator.open_STM_board();
 
 //	And for reading response from device:
-	char readBuffer[10];
+
 	char run = 1;
 	string consoleInput;
 	while(run){
@@ -80,63 +161,13 @@ int main() {
 		istringstream buffer(consoleInput);
 		buffer >> pwmWidth;
 
-		cout << "pwmWidth: " << pwmWidth << endl;
+		cout << "Try to send pulse width ..." << pwmWidth << endl;
 
-		cout << "Try to send pulse width ..." << endl;
+		mySTMCommunicator.writeByte(pwmWidth);
 
-		char writeBytes[1];
-		memcpy((void *)writeBytes, (void*)&pwmWidth, 1);
-
-		bool byteWritten = false;
-		while(!byteWritten){
-			usleep(1000);
-			int numberBytesWritten = write(fd1, (void*)&(writeBytes[0]), 1);
-			if(numberBytesWritten == 1){
-				byteWritten = true;
-			} else {
-				cout << "numberBytesWritten byte is smaller zero: " << numberBytesWritten << endl;
-				return -1;
-			}
-			cout << "write-check ... number bytes written: " << numberBytesWritten << endl;
-		}
-
-		cout << "Try to receive byte ...\n";
-
-		bool byteRead = false;
-		while(!byteRead){
-			usleep(10000);
-			int numberBytesRead = read(fd1, (void*)&(readBuffer[0]), 1);
-			if(numberBytesRead > 1){
-				cout << "read more than 2 bytes at the same time!" << endl;
-				return -1 ;
-			}
-			else if(numberBytesRead < 0){
-				cout << "############# error during read ################" << endl;
-				continue;
-			} else if(numberBytesRead == 1){
-				byteRead = true;
-			}
-			if(readBuffer[0] == 10 || readBuffer[0] == 13){
-				cout << "Stop/Start Byte received!" << endl;
-				byteRead = false;
-			}
-			cout << "read-check; numRead is " << numberBytesRead << endl;
-		}
-
-		uint8_t receivedPWMWidth = 0;
-		memcpy((void *)&receivedPWMWidth, (void*)readBuffer, 1);
-
-		if(receivedPWMWidth == pwmWidth){
-			cout << "Same value!" << endl;
-		} else {
-			cout << "Not the same value! receivedPWMWidth: " << (uint)(receivedPWMWidth) << endl;
-		}
 	}
 
 //	At the end, close the connection:
-	close(fd1);
 	return 0;
 }
-
-//TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
 
